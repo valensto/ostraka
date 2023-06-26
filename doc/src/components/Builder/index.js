b2b92cfc9c1d6a1af27499ab69be64ff0ece98a8
Valensto <hi@valensto.com>
2023-06-26 19:40:57 +0200

    update docusaurus config for deployment

--- doc/src/components/Builder/index.js
+++ doc/src/components/Builder/index.js
@@ -1,8 +1,8 @@
 import React, {useCallback, useEffect, useState} from "react"
 import useWindowWidth from "../../hooks/useWindowWidth"
 import clsx from "clsx"
-import Highlight from "./Highlight.js"
-import Chevron from "./chevron.js"
+import Highlight from "./highlight"
+import Chevron from "./chevron"
 import styles from "./styles.module.css"
 import {Section} from "../Section";
 
